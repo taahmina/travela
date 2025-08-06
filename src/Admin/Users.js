53863faf0a00eba89939d1895c4665aa396a4ad5
ImtiazAhmedArefin <imtiazahmed.arefin@gmail.com>
2025-08-06 13:01:09 +0600

    user crude(add/edit/delete)

--- src/Admin/Users.js
+++ src/Admin/Users.js
@@ -1,24 +1,94 @@
 import React, { useEffect,useState} from 'react';
 import axios from 'axios';
 import Adminlayout from '../layout/Adminlayout';
-
-import{Link} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 
 
 function Users(){
     const[users,setUsers]=useState([]);
-    
+    const[show,setShow]=useState(false);
+
+    const handleClose = () => {
+      setShow(false)
+    };
+    const handleShow = ()=>{
+      setInputs({
+        id:'',
+         name:'',
+          contact_no:'',
+           email:'',
+            password:'',
+
+      });
+
+    setShow(true);
+        }
+
+useEffect(()=>{
+  getDatas();
+},[]);
+
+
+const getDatas=()=>{
+
+
 
     axios.get(`http://localhost/travela-api/users.php`).then(function(response){
         setUsers(response.data);
     });
-    return(
-        <Adminlayout>
-            <div className='container'>
+    }
+const[inputs, setInputs]=useState([]);
+const handleChange=(event)=>{
+   const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}));
+}
+const handleSubmit = async(e) => {
+    e.preventDefault();
+    try{
+      let url='';
+      if(inputs.id!=''){
+        url='http://localhost/travela-api/users_update.php';
+      }else{
+        url='http://localhost/travela-api/users_add.php';
+      }
+      let response= await axios({
+          method: 'post',
+          responsiveTYpe: 'json',
+          url: url,
+          data: inputs
+      });
+      getDatas();
+      handleClose()
+    } 
+    catch(e){
+      console.log(e);
+    }
+  }
+  /* function for edit */
+  const showEdit=(e) => {
+    setInputs(e);
+    setShow(true);
+  }
+
+  const deleteUser = (id) => {
+    axios.delete(`http://localhost/travela-api/users_delete.php?id=${id}`).then(function(response){
+      getDatas();
+    });
+  }
+
+
+  return (
+    <Adminlayout>
+      <div className='container'>
         <h1>User</h1>
-        <Link to={'/admin/add-user'} className="btn btn-primary">Add New</Link>
+        <Button variant="primary" onClick={handleShow}>
+          Add New
+        </Button>
         <table className='mt-5 table table-bordered'>
+          <thead>
           <tr>
             <th>#SL</th>
             <th>Name</th>
@@ -26,6 +96,8 @@ function Users(){
             <th>Email</th>
             <th>Action</th>
           </tr>
+          </thead>
+          <tbody>
           {users && users.map((d, key) =>
             <tr key={key}>
               <td className="text-bold-500">{key+1}</td>
@@ -33,13 +105,59 @@ function Users(){
               <td>{d.contact_no}</td>
               <td>{d.email}</td>
               <td>
-                  
+                  <Button variant="primary" onClick={()=>{showEdit(d)}}>Edit</Button>
+                  <Button variant="danger" onClick={()=>{deleteUser(d.id)}}>Delete</Button>
               </td>
             </tr>
           )}
+          </tbody>
         </table>
+
+
+      <Modal show={show} onHide={handleClose}>
+        <form onSubmit={handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add User</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <div className='form-group'>
+                  <label htmlFor='name'>Name</label>
+                  <input type='text' defaultValue={inputs.name} className='form-control' name="name" id='name' onChange={handleChange}/>
+              </div>
+              <div className='form-group'>
+                  <label htmlFor='email'>Email</label>
+                  <input type='text' defaultValue={inputs.email} className='form-control' name="email" id='email' onChange={handleChange}/>
+              </div>
+              <div className='form-group'>
+                  <label htmlFor='contact_no'>Contact</label>
+                  <input type='text' defaultValue={inputs.contact_no} className='form-control' name="contact_no" id='contact_no' onChange={handleChange}/>
+              </div>
+              <div className='form-group'>
+                  <label htmlFor='password'>Password</label>
+                  <input type='text' defaultValue={inputs.password} className='form-control' name='password' id='password' onChange={handleChange}/>
+              </div>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" type='submit'>
+              Save
+            </Button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+
       </div>
-        </Adminlayout>
-    );
- }
- export default Users;
+    </Adminlayout>
+    
+  );
+}
+
+
+export default Users;
+
+
+
+
+
+
+
